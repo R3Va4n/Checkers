a05f4a7fb83211e250d583c0a16af3df10bf2a88
R3V4N <mausemaelken@gmx.de>
2024-01-25 17:46:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,28 @@
+import numpy as np
 class main():
-    pass
+    
+    current_board = None
+
+    def make_starting_board(self) -> np.array:
+        a = []
+        b = []
+        for i in range(8):
+            a.append(0)
+        for i in range(8):
+            b.append(a)
+        for x in b:
+            for y in x:
+                if (x.length() <= 2 and x.length() % 2 == 0 and y % 2 == 1):
+                    y == -1
+                
+                elif(x >= 5 and x % 2 == 1 and y % 2 == 0):
+                    y == 1
+        for i in b:
+            print(i)
+
+
     def __init__(self) -> None:
-        pass
+        current_board = []
+        self.make_starting_board()
+
+a = main()
